6145e56d25284f1e9584a820ed2d10803cd3d523
agnoorsaivarun <agnoorsaivarun1@gmail.com>
2022-12-23 00:11:31 +0530

    edited

--- src/Components/AddNewProperty/basicinfo.js
+++ src/Components/AddNewProperty/basicinfo.js
@@ -3,11 +3,20 @@ import { useNavigate } from "react-router-dom";
 
 const BasicInfo = ({ setStep, propertyData, setPropertyData }) => {
     const navigate = useNavigate()
+    const handleChange = (e) => {
+        console.log(propertyData);
+        const name = e.target.name;
+        const value = e.target.value;
+        setPropertyData(previousData => {
+            return {...previousData, [name]: value}
+        });
+        console.log(propertyData);
+    }
     return (
         <div id="basicinfo">
             <div className="type">
                 <label htmlFor="propertytype">Property Type</label>
-                <select required={true} id="propertype" name="property_type" value={propertyData.property_type} onChange={(e) => { setPropertyData({ ...propertyData, property_type: e.target.value }) }}>
+                <select required={true} id="propertype" name="property_type" value={propertyData.property_type} onChange={(e) => { handleChange(e) }}>
                     <option>private</option>
                     <option>public</option>
                     <option>government</option>
@@ -15,25 +24,25 @@ const BasicInfo = ({ setStep, propertyData, setPropertyData }) => {
             </div>
             <div className="neg">
                 <label htmlFor="negotiable">Is Negotiable?</label>
-                <select required={true} id="negotiable" name="negotiable" value={propertyData.negotiable} onChange={(e) => { setPropertyData({ ...propertyData, negotiable: e.target.value }) }}>
+                <select required={true} id="negotiable" name="negotiable" value={propertyData.negotiable} onChange={(e) => { handleChange(e) }}>
                     <option>yes</option>
                     <option>no</option>
                 </select>
             </div>
             <div className="price">
                 <label htmlFor="price">Price</label>
-                <input required={true} type="number" id="price" name="price" value={propertyData.price} onChange={(e) => { setPropertyData({ ...propertyData, price: e.target.value }) }} />
+                <input required={true} type="number" id="price" name="price" value={propertyData.price} onChange={(e) => { handleChange(e) }} />
             </div>
             <div className="ownership">
                 <label htmlFor="ownership">Ownership</label>
-                <select required={true} id="ownership" name="ownership" value={propertyData.ownership} onChange={(e) => { setPropertyData({ ...propertyData, ownership: e.target.value }) }}>
+                <select required={true} id="ownership" name="ownership" value={propertyData.ownership} onChange={(e) => { handleChange(e) }}>
                     <option>self</option>
                     <option>family</option>
                 </select>
             </div>
             <div className="age">
                 <label htmlFor="propertyage">Property Age</label>
-                <select required={true} id="propertyage" name="property_age" value={propertyData.property_age} onChange={(e) => { setPropertyData({ ...propertyData, property_age: e.target.value }) }}>
+                <select required={true} id="propertyage" name="property_age" value={propertyData.property_age} onChange={(e) => { handleChange(e) }}>
                     <option>1-3 years</option>
                     <option>4-8 years</option>
                     <option>9-15 years</option>
@@ -41,14 +50,14 @@ const BasicInfo = ({ setStep, propertyData, setPropertyData }) => {
             </div>
             <div className="appr">
                 <label htmlFor="propertyapproved">Property Approved</label>
-                <select required={true} id="propertyapproved" name="property_approved" value={propertyData.property_approved} onChange={(e) => { setPropertyData({ ...propertyData, property_approved: e.target.value }) }}>
+                <select required={true} id="propertyapproved" name="property_approved" value={propertyData.property_approved} onChange={(e) => { handleChange(e) }}>
                     <option>yes</option>
                     <option>no</option>
                 </select>
             </div>
             <div className="desc">
                 <label htmlFor="propertydescription">Property Description</label>
-                <select id="propertydescription" name="property_description" required={true} value={propertyData.property_description} onChange={(e) => { setPropertyData({ ...propertyData, property_description: e.target.value }) }}>
+                <select id="propertydescription" name="property_description" required={true} value={propertyData.property_description} onChange={(e) => { handleChange(e) }}>
                     <option>Flat Bungalow</option>
                     <option>Apartment</option>
                     <option>Plot</option>
@@ -56,7 +65,7 @@ const BasicInfo = ({ setStep, propertyData, setPropertyData }) => {
             </div>
             <div className="loan">
                 <label htmlFor="bankloan">Any Bank Loan?</label>
-                <select required={true} id="bankloan" name="bank_loan" value={propertyData.bank_loan} onChange={(e) => { setPropertyData({ ...propertyData, bank_loan: e.target.value }) }}>
+                <select required={true} id="bankloan" name="bank_loan" value={propertyData.bank_loan} onChange={(e) => { handleChange(e) }}>
                     <option>yes</option>
                     <option>no</option>
                 </select>
--- src/Components/AddNewProperty/generalinfo.js
+++ src/Components/AddNewProperty/generalinfo.js
@@ -1,19 +1,28 @@
 import React from "react";
 
 const GeneralInfo = ({ setStep, propertyData, setPropertyData }) => {
+    const handleChange = (e) => {
+        console.log(propertyData);
+        const name = e.target.name;
+        const value = e.target.value;
+        setPropertyData(previousData => {
+            return {...previousData, [name]: value}
+        });
+        console.log(propertyData);
+    }
     return (
         <div id="generalinfo">
             <div className="name">
                 <label htmlFor="name">Name</label>
-                <input required={true} id="name" name="name" value={propertyData.name} onChange={(e) => { setPropertyData({ ...propertyData, name: e.target.value }) }} />
+                <input required={true} id="name" name="name" value={propertyData.name} onChange={(e) => { handleChange(e) }} />
             </div>
             <div className="mobile">
                 <label htmlFor="mobile">Mobile</label>
-                <input type="number" required={true} id="mobile" name="mobile" value={propertyData.mobile} onChange={(e) => { setPropertyData({ ...propertyData, mobile: e.target.value }) }} />
+                <input type="number" required={true} id="mobile" name="mobile" value={propertyData.mobile} onChange={(e) => { handleChange(e) }} />
             </div>
             <div className="posted_by">
                 <label htmlFor="posted_by">Posted By</label>
-                <select required={true} id="posted_by" name="posted_by" value={propertyData.posted_by} onChange={(e) => { setPropertyData({ ...propertyData, posted_by: e.target.value }) }}>
+                <select required={true} id="posted_by" name="posted_by" value={propertyData.posted_by} onChange={(e) => { handleChange(e) }}>
                     <option>self</option>
                     <option>family</option>
                     <option>owner</option>
@@ -21,7 +30,7 @@ const GeneralInfo = ({ setStep, propertyData, setPropertyData }) => {
             </div>
             <div className="sale_type">
                 <label htmlFor="sale_type">Sale Type</label>
-                <select required={true} id="sale_type" name="sale_type" value={propertyData.sale_type} onChange={(e) => { setPropertyData({ ...propertyData, sale_type: e.target.value }) }}>
+                <select required={true} id="sale_type" name="sale_type" value={propertyData.sale_type} onChange={(e) => { handleChange(e) }}>
                     <option>private</option>
                     <option>lease</option>
                     <option>ownership</option>
@@ -29,14 +38,14 @@ const GeneralInfo = ({ setStep, propertyData, setPropertyData }) => {
             </div>
             <div className="featured_package">
                 <label htmlFor="featured_package">Featured Package</label>
-                <select required={true} id="featured_package" name="featured_package" value={propertyData.featured_package} onChange={(e) => { setPropertyData({ ...propertyData, featured_package: e.target.value }) }}>
+                <select required={true} id="featured_package" name="featured_package" value={propertyData.featured_package} onChange={(e) => { handleChange(e) }}>
                     <option>yes</option>
                     <option>no</option>
                 </select>
             </div>
             <div className="ppd_package">
                 <label htmlFor="ppd_package">PPD Package</label>
-                <select required={true} id="ppd_package" name="ppd_package" value={propertyData.ppd_package} onChange={(e) => { setPropertyData({ ...propertyData, ppd_package: e.target.value }) }}>
+                <select required={true} id="ppd_package" name="ppd_package" value={propertyData.ppd_package} onChange={(e) => { handleChange(e) }}>
                     <option>1 ppd</option>
                     <option>2 ppd</option>
                     <option>3 ppd</option>
