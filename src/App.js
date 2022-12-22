3deb46369b3f4089927e4a1e07bdd101cd4ee218
agnoorsaivarun <agnoorsaivarun1@gmail.com>
2022-12-23 00:44:24 +0530

    new property approach is changed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import Header from "./Components/Header/header";
 import Login from "./Components/Signup-Login/Login";
 import Signup from "./Components/Signup-Login/Signup";
 import Protected from "./Components/Protected/protected"
-import AddNewProperty from "./Components/AddNewProperty/addnewproperty";
+import BasicInfo from "./Components/AddNewProperty/BasicInfo";
+import LocationInfo from "./Components/AddNewProperty/locationinfo";
+import GeneralInfo from "./Components/AddNewProperty/generalinfo";
+import PropertyDetails from "./Components/AddNewProperty/propertyDetails";
 
 function App() {
   return (
@@ -15,10 +17,34 @@ function App() {
     <Route path="/" element={<Login/>} />
     <Route path="/Signup" element={<Signup/>} />
     <Route
-          path="/addnewproperty"
+          path="/basicinfo"
           element={
             <Protected>
-              <AddNewProperty />
+              <BasicInfo />
+            </Protected>
+          }
+        ></Route>
+        <Route
+          path="/propertydeatils"
+          element={
+            <Protected>
+              <PropertyDetails />
+            </Protected>
+          }
+        ></Route>
+        <Route
+          path="/generalinfo"
+          element={
+            <Protected>
+              <GeneralInfo />
+            </Protected>
+          }
+        ></Route>
+        <Route
+          path="/locationinfo"
+          element={
+            <Protected>
+              <LocationInfo />
             </Protected>
           }
         ></Route>
